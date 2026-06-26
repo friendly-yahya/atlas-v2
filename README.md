923921b84226cf1a393124357ba2fdbb029e9cef
friendly-yahya <yahyafarkli63@gmail.com>
2026-06-26 09:45:26 +0100

    Initial Flutter project setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# atlas_paragliding_v2
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/atlas_paragliding_v2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.atlas_paragliding_v2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
